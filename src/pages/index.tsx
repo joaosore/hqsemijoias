46370b53017ecd7927a8e921bc27e8b074e7fd99
João Marcos Soré de Morais <jaumcj@gmail.com>
2022-11-05 12:08:35 -0300

    a

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { GetServerSideProps } from 'next';
+
+import Head from 'next/head';
+
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início</title>
+      </Head>
+      <div className={styles.container}>
+        <img src="./img/logo.png" alt="" />
+        <h1>Em Breve!!!</h1>
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  return {
+    props: {
+      
+    },
+  };
+};
